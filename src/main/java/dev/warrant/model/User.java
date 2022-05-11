501237cd135506e14ee612d7da3065bdf80bd4d8
stanleyphu <phu.stanley@gmail.com>
2022-05-11 16:58:22 -0700

    Update User to include email field

--- src/main/java/dev/warrant/model/User.java
+++ src/main/java/dev/warrant/model/User.java
@@ -1,6 +1,10 @@
 package dev.warrant.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(value = { "createdAt", "updatedAt" })
 public class User {
+    private String email;
     private String userId;
 
     public User() {
@@ -8,7 +12,13 @@ public class User {
     }
 
     public User(String userId) {
+        this.email = "";
         this.userId = userId;
+    }
+    
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
     }
 
     public String getUserId() {
@@ -17,5 +27,13 @@ public class User {
 
     public void setUserId(String userId) {
         this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
