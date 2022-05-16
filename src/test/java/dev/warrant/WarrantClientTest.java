3c0bc5b3cc25cd1623664babb340bd622caf58bc
Aditya Kajla <akajla09@gmail.com>
2022-05-15 21:48:22 -0700

    Add unit tests for users & tenants

--- src/test/java/dev/warrant/WarrantClientTest.java
+++ src/test/java/dev/warrant/WarrantClientTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import dev.warrant.exception.WarrantException;
+import dev.warrant.model.Tenant;
 import dev.warrant.model.User;
 
 public class WarrantClientTest {
@@ -23,16 +24,67 @@ public class WarrantClientTest {
     public void init() throws IOException, InterruptedException {
         httpClient = Mockito.mock(HttpClient.class);
         httpResponse = Mockito.mock(HttpResponse.class);
-        Mockito.when(httpClient.send(Mockito.any(HttpRequest.class), Mockito.any(BodyHandler.class))).thenReturn(httpResponse);
+        Mockito.when(httpClient.send(Mockito.any(HttpRequest.class), Mockito.any(BodyHandler.class)))
+                .thenReturn(httpResponse);
     }
 
     @Test
     public void testCreateUser() throws WarrantException {
         Mockito.when(httpResponse.statusCode()).thenReturn(200);
-        Mockito.when(httpResponse.body()).thenReturn("{\"userId\":\"sdf872934sdf\"}");
+        Mockito.when(httpResponse.body())
+                .thenReturn("{\"userId\":\"sdf872934sdf\", \"createdAt\": \"2022-05-14T02:40:25Z\"}");
 
         WarrantClient warrantClient = new WarrantClient(WarrantConfig.withApiKey("sample_key"), httpClient);
         User newUser = warrantClient.createUser();
         Assertions.assertEquals("sdf872934sdf", newUser.getUserId());
+        Assertions.assertNull(newUser.getEmail());
+    }
+
+    @Test
+    public void testCreateUserWithId() throws WarrantException {
+        Mockito.when(httpResponse.statusCode()).thenReturn(200);
+        Mockito.when(httpResponse.body())
+                .thenReturn("{\"userId\":\"sdf872935sdf\", \"createdAt\": \"2022-05-14T02:40:25Z\"}");
+
+        WarrantClient warrantClient = new WarrantClient(WarrantConfig.withApiKey("sample_key"), httpClient);
+        User newUser = warrantClient.createUser("sdf872935sdf");
+        Assertions.assertEquals("sdf872935sdf", newUser.getUserId());
+        Assertions.assertNull(newUser.getEmail());
+    }
+
+    @Test
+    public void testCreateUserWithIdAndEmail() throws WarrantException {
+        Mockito.when(httpResponse.statusCode()).thenReturn(200);
+        Mockito.when(httpResponse.body()).thenReturn(
+                "{\"userId\":\"sdf872934sdf\", \"email\": \"test@test.com\", \"createdAt\": \"2022-05-14T02:40:25Z\"}");
+
+        WarrantClient warrantClient = new WarrantClient(WarrantConfig.withApiKey("sample_key"), httpClient);
+        User newUser = warrantClient.createUser("sdf882934sdf", "test@test.com");
+        Assertions.assertEquals("sdf872934sdf", newUser.getUserId());
+        Assertions.assertEquals("test@test.com", newUser.getEmail());
+    }
+
+    @Test
+    public void testCreateTenant() throws WarrantException {
+        Mockito.when(httpResponse.statusCode()).thenReturn(200);
+        Mockito.when(httpResponse.body()).thenReturn(
+                "{\"tenantId\": \"913241cf-740\", \"name\": null, \"createdAt\": \"2022-05-16T04:33:39Z\" }");
+
+        WarrantClient warrantClient = new WarrantClient(WarrantConfig.withApiKey("sample_key"), httpClient);
+        Tenant newTenant = warrantClient.createTenant();
+        Assertions.assertEquals("913241cf-740", newTenant.getTenantId());
+        Assertions.assertNull(newTenant.getName());
+    }
+
+    @Test
+    public void testCreateTenantWithId() throws WarrantException {
+        Mockito.when(httpResponse.statusCode()).thenReturn(200);
+        Mockito.when(httpResponse.body())
+                .thenReturn("{\"tenantId\": \"913241cf\", \"name\": null, \"createdAt\": \"2022-05-16T04:33:39Z\" }");
+
+        WarrantClient warrantClient = new WarrantClient(WarrantConfig.withApiKey("sample_key"), httpClient);
+        Tenant newTenant = warrantClient.createTenant("913241cf");
+        Assertions.assertEquals("913241cf", newTenant.getTenantId());
+        Assertions.assertNull(newTenant.getName());
     }
 }
