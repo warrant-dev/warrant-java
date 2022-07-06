c3f08d14ed197407162778953f613c36c3b28fe6
Aditya Kajla <akajla09@users.noreply.github.com>
2022-07-06 14:52:42 -0700

    Change user to subject in createWarrant and isAuthorized (#5)
    * Change user to subject in createWarrant and isAuthorized
    
    * Add back relation to Subject

---
+++ src/main/java/dev/warrant/model/Subject.java
@@ -0,0 +1,46 @@
+package dev.warrant.model;
+
+public class Subject {
+    private String objectType;
+    private String objectId;
+    private String relation;
+
+    public Subject() {
+        // For json serialization
+    }
+
+    public Subject(String objectType, String objectId, String relation) {
+        this.objectType = objectType;
+        this.objectId = objectId;
+        this.relation = relation;
+    }
+
+    public Subject(String objectType, String objectId) {
+        this.objectType = objectType;
+        this.objectId = objectId;
+    }
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+}
