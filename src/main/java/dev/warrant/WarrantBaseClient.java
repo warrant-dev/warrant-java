3df4e495a43bc4405afd8ae247048d031e6e7fb6
Aditya Kajla <akajla09@gmail.com>
2023-07-11 12:23:20 -0700

    v2.2.0: Update deps + add support for cursor-based pagination

--- src/main/java/dev/warrant/WarrantBaseClient.java
+++ src/main/java/dev/warrant/WarrantBaseClient.java
@@ -26,7 +26,7 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
 
 public class WarrantBaseClient {
-    public static final String SDK_VERSION = "2.1.0";
+    public static final String SDK_VERSION = "2.2.0";
     public static final String USER_AGENT = "warrant-java/" + SDK_VERSION;
 
     final HttpClient client;
